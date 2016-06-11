6e662da595ea807f89c3a848af25081496fbd5a1
liuhongliang <1029765111@qq.com>
2016-06-11 16:37:15 +0800

    Set a WebHost For This Project

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 
 namespace ConsoleApplication
 {
@@ -9,7 +10,13 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            System.Console.WriteLine("This is a demo");
+           // System.Console.WriteLine("This is a demo");
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
         }
     }
 }
